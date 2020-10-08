8861c10e5f14a386575a2ae3e7561095a5868d45
dkoziol123 <70176289+dkoziol123@users.noreply.github.com>
2020-10-08 17:31:30 -0500

    Update views.py

--- LogosLibrary/LogosLibrary/views.py
+++ LogosLibrary/LogosLibrary/views.py
@@ -12,35 +12,5 @@ def home(request):
                  {'crm': home})
 
 @login_required
-def customer_l(request):
-    customer = Customer.objects.filter(created_date__lte=timezone.now())
-    return render(request, 'crm/customer_list.html',
-                 {'customers': customer})
 
-@login_required
-def customer_d(request, pk):
-   customer = get_object_or_404(Customer, pk=pk)
-   
-
-@login_required
-def customer_e(request, pk):
-   customer = get_object_or_404(Customer, pk=pk)
-   
-
-@login_required
-def customer_service(request):
-   services = Service.objects.filter(created_date__lte=timezone.now())
-   return render(request, 'crm/service_list.html', {'services': services})
-
-@login_required
-def customer_service_new(request):
-   
-
-@login_required
-def customer_service_edit(request, pk):
-   service = get_object_or_404(Service, pk=pk)
-   
-@login_required
-def summary(request, pk):
-    customer = get_object_or_404(Customer, pk=pk)
     
